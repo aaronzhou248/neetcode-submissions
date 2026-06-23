35ed9646d05b091f604b9a45be9a3e0e8fe2e9b4
Aaron Zhou <30437714+aaronzhou248@users.noreply.github.com>
2026-06-22 18:24:29 -0700

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.go
@@ -0,0 +1,35 @@
+func longestConsecutive(nums []int) int {
+	dictionary := map[int]bool{} // this will map a number to whether or not we've used it.
+	maxLen := 0
+
+	for _, num := range(nums) {
+		dictionary[num] = true
+	}
+
+	for _, num := range(nums) {
+		if isUnused, succ := dictionary[num]; !succ || !isUnused {
+			continue
+		}
+		currLength := 1
+		for left := num - 1; left >= -1000000000; left-- {
+			if isUnused, succ := dictionary[left]; !succ || !isUnused {
+				break
+			}
+			dictionary[left] = false
+			currLength += 1
+		}
+		for right := num + 1; right <= 1000000000; right++ {
+			if isUnused, succ := dictionary[right]; !succ || !isUnused {
+				break
+			}
+			dictionary[right] = false
+			currLength += 1
+		}
+
+		if currLength > maxLen {
+			maxLen = currLength
+		}
+	}
+
+	return maxLen
+}

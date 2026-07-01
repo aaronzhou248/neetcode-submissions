ae2dbf84f3a8947eef294369175862984d3fd511
Aaron Zhou <30437714+aaronzhou248@users.noreply.github.com>
2026-06-30 21:40:34 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.go
@@ -0,0 +1,11 @@
+func canAttendMeetings(intervals []Interval) bool {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].start < intervals[j].start
+	})
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i].start < intervals[i-1].end {
+			return false // overlap: this one starts before the previous ends
+		}
+	}
+	return true
+}

cbeecc458371d46eb5bef680375232d6c3c9c06e
Aaron Zhou <30437714+aaronzhou248@users.noreply.github.com>
2026-07-06 20:12:27 -0700

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.go
@@ -0,0 +1,44 @@
+func robHelp(nums []int) int {
+	switch len(nums) {
+		case 0:
+			return 0
+		case 1:
+			return nums[0]
+		case 2:
+			return max(nums[0], nums[1])
+	}
+
+	n := len(nums)
+	type tup struct{
+		this int
+		last int
+	}
+
+	sol := make([]tup, n)
+
+	for i, num := range nums {
+		if i == 0 {
+			sol[i] = tup{num, 0}
+			continue
+		} else if i == 1 {
+			sol[i] = tup{num, nums[0]}
+			continue
+		}
+
+		sol[i] = tup{num + max(sol[i-2].this, sol[i-2].last), sol[i-1].this}
+	}
+
+	return max(sol[n-1].this, sol[n-1].last)
+}
+
+func rob(nums []int) int {
+	switch len(nums) {
+		case 0:
+			return 0
+		case 1:
+			return nums[0]
+		case 2:
+			return max(nums[0], nums[1])
+	}
+	return max(robHelp(nums[:len(nums) - 1]), robHelp(nums[1:]))
+}
